7438fd3a4d5cd8cd49bba87bd6b2bcff77a3c1f0
Diana Kharchenko <vitadiana2@gmail.com>
2024-01-20 18:31:44 +0200

    all

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+class Storage {
+#items = [];
+constructor(items) {
+    this.#items = items
+}
+
+getItems() {
+    return this.#items;
+}
+
+addItem(newItem) {
+    this.#items.push(newItem);
+}
+
+removeItem(itemToRemove) {
+    const indexToRemove = this.#items.indexOf(itemToRemove);
+    if (indexToRemove !== -1) {
+      this.#items.splice(indexToRemove, 1);
+    }
+  }
+}
+
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
